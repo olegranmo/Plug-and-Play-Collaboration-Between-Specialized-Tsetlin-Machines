45fc9f46c004cb062797c19410b462cd104f24c7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-08-27 22:28:27 +0200

    Update

---
+++ CIFAR10AdaptiveThresholding.py
@@ -0,0 +1,65 @@
+import argparse
+from tmu.models.classification.vanilla_classifier import TMClassifier
+import numpy as np
+from keras.datasets import cifar10
+import cv2
+from tmu.preprocessing.standard_binarizer.binarizer import StandardBinarizer
+from time import time
+
+_LOGGER = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_clauses", default=8000, type=int)
+    parser.add_argument("--T", default=2000, type=int)
+    parser.add_argument("--s", default=10.0, type=float)
+    parser.add_argument("--max_included_literals", default=32, type=int)
+    parser.add_argument("--device", default="GPU", type=str)
+    parser.add_argument("--weighted_clauses", default=True, type=bool)
+    parser.add_argument("--epochs", default=100, type=int)
+    parser.add_argument("--type_i_ii_ratio", default=1.0, type=float)
+    parser.add_argument("--patch_size", default=10, type=int)
+
+    args = parser.parse_args()
+
+    (X_train_org, Y_train), (X_test_org, Y_test) = cifar10.load_data()
+    
+    X_train = np.copy(X_train_org)
+    X_test = np.copy(X_test_org)
+    
+    Y_train=Y_train.reshape(Y_train.shape[0])
+    Y_test=Y_test.reshape(Y_test.shape[0])
+
+    for i in range(X_train.shape[0]):
+            for j in range(X_train.shape[3]):
+                    X_train[i,:,:,j] = cv2.adaptiveThreshold(X_train_org[i,:,:,j], 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2) #cv2.adaptiveThreshold(X_train[i,:,:,j], 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 5)
+
+    for i in range(X_test.shape[0]):
+            for j in range(X_test.shape[3]):
+                X_test[i,:,:,j] = cv2.adaptiveThreshold(X_test_org[i,:,:,j], 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)#cv2.adaptiveThreshold(X_test[i,:,:,j], 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 5)
+
+    tm = TMClassifier(
+        number_of_clauses=args.num_clauses,
+        T=args.T,
+        s=args.s,
+        max_included_literals=args.max_included_literals,
+        platform=args.device,
+        weighted_clauses=args.weighted_clauses,
+        type_i_ii_ratio=args.type_i_ii_ratio,
+        patch_dim=(args.patch_size, args.patch_size)
+    )
+
+    for epoch in range(args.epochs):
+        start_training = time()
+        tm.fit(X_train, Y_train)
+        stop_training = time()
+
+        start_testing = time()
+        Y_test_predicted, Y_test_scores = tm.predict(X_test, return_class_sums=True)
+        stop_testing = time()
+
+        result_test = (Y_test_scores.argmax(axis=1) == Y_test).mean()
+
+        print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
+
+        np.savetxt("CIFAR10AdaptiveThresholding_%d_%d_%d_%.1f_%d_%d.txt" % (epoch, args.num_clauses, args.T, args.s, args.patch_size, args.max_included_literals), Y_test_scores, delimiter=',')
