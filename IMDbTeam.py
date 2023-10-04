5f66b0128d9b622a387220037684cece293474dc
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-10-04 21:54:37 +0200

    Experimenting with text categorization

--- IMDbTeam.py
+++ IMDbTeam.py
@@ -16,10 +16,10 @@ test_x, test_y = test
 
 Y_test = test_y.astype(np.uint32)
 
-#Y_test_scores_1 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_1_1_32_1_word_0.75_4899.txt" % (args.epoch), delimiter=',')
-#Y_test_scores_2 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_2_2_32_1_word_0.75_5000.txt" % (args.epoch), delimiter=',')
-#Y_test_scores_3 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_3_3_32_1_word_0.75_5000.txt" % (args.epoch), delimiter=',')
-#Y_test_scores_4 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_4_4_32_1_char_wb_0.75_3000.txt" % (args.epoch), delimiter=',')
+Y_test_scores_1 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_1_1_32_1_word_0.75_4899.txt" % (args.epoch), delimiter=',')
+Y_test_scores_2 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_2_2_32_1_word_0.75_5000.txt" % (args.epoch), delimiter=',')
+Y_test_scores_3 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_3_3_32_1_word_0.75_5000.txt" % (args.epoch), delimiter=',')
+Y_test_scores_4 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_4_4_32_1_char_wb_0.75_3000.txt" % (args.epoch), delimiter=',')
 
 # Y_test_scores_1 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_1_2_32_1_word_0.75_4899.txt" % (args.epoch), delimiter=',')
 # Y_test_scores_2 = np.loadtxt("class_sums/IMDBAnalyzer_%d_10000_8000_2.00_1_2_32_1_word_0.75_5000.txt" % (args.epoch), delimiter=',')
